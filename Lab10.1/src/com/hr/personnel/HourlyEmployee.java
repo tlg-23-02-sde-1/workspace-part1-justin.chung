7020b67eb25c7c3e49b71bf6947e81bebbcf951f
Justin Chung <justin.chung@tlgcohort.com>
2023-02-22 15:55:57 -0600

    Lab 10.1

--- Lab10.1/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1/src/com/hr/personnel/HourlyEmployee.java
@@ -8,10 +8,13 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
     // fields
+    public static final double FEDERAL_MINIMUM_WAGE = 15.00;
     private double rate;
     private double hours;
 
@@ -23,7 +26,8 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
+    throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -46,7 +50,10 @@ public class HourlyEmployee extends Employee {
         return rate;
     }
 
-    public void setRate(double rate) {
+    public void setRate(double rate) throws IllegalWageException {
+        if(rate < FEDERAL_MINIMUM_WAGE) {
+            throw new IllegalWageException("Hourly rate must be at least " + FEDERAL_MINIMUM_WAGE );
+        }
         this.rate = rate;
     }
 
